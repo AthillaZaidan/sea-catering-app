214c9de6ba23425488912e0b9f4736b37ff9c031
AthillaZaidan <athillazaidanstudy@gmail.com>
2025-06-29 00:08:37 +0700

    feat: add nav

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,11 @@
 const mainHeader = document.querySelector('header.header');
-
 const SCROLL_THRESHOLD = 20; 
 
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > SCROLL_THRESHOLD) {
         mainHeader.classList.add('scrolled');
+        mainNavigation.classList.add('scrolled');
     } else {
         mainHeader.classList.remove('scrolled');
     }
